455db48e82b9549c2d1210336600d12838351730
fatawaimamalmuftin <muftinmarmut@gmail.com>
2026-06-14 15:50:53 +0700

    chore: make file route package

---
+++ BeliHemat-react/src/pages/CheckOutSucces.jsx
@@ -0,0 +1,9 @@
+export default function CheckOutSucces() {
+    return (
+        <>
+            <main className="flex flex-col items-center min-h-screen bg-slate-100 gap-6">
+                <h1 className="text-4xl font-bold text-emerald-600">Ini Halaman Tentang (CheckOutSucces)</h1>
+            </main>
+        </>
+    )
+}
---
+++ BeliHemat-react/src/pages/ProfileMyOrder.jsx
@@ -0,0 +1,9 @@
+export default function ProfileMyOrder() {
+    return (
+        <>
+            <main className="flex flex-col items-center min-h-screen bg-slate-100 gap-6">
+                <h1 className="text-4xl font-bold text-emerald-600">Ini Halaman Tentang (ProfileMyOrder)</h1>
+            </main>
+        </>
+    )
+}
