22a0dc6e9fca2686e705bafaed5ea48d199ed104
fatawaimamalmuftin <muftinmarmut@gmail.com>
2026-06-15 22:22:49 +0700

    docs: wrong repo before

---
+++ src/componen/Navbar.jsx
@@ -0,0 +1,122 @@
+import { Link, useNavigate } from 'react-router-dom';
+import {
+    MapPin,
+    Search,
+    Bell,
+    User,
+    Heart,
+    ShoppingCart,
+    Menu,
+} from 'lucide-react';
+
+export default function Navbar() {
+    const nafigasi = useNavigate();
+
+    const handleNavigasi = (event) => {
+        const pathTujuan = event.target.value
+        if (pathTujuan) {
+            nafigasi(pathTujuan);
+        }
+    }
+    return (
+        <>
+            <header className="top-0 flex justify-between items-center bg-[#4F39F6] w-full px-[120px] py-[5px]">
+                <div className="flex items-center gap-[6px]">
+                    <MapPin className="text-white" size={16} />
+                    <span className="text-white text-[12px]">
+                        Kirim ke: Jakarta Selatan
+                    </span>
+                </div>
+
+                <div className="flex gap-[24px]">
+                    <div className="text-white text-[12px]">
+                        🚀 Gratis ongkir di atas Rp 100.000
+                    </div>
+                    <div className="text-white text-[12px]">
+                        📞 0800-1234-5678 (Gratis)
+                    </div>
+                </div>
+            </header>
+
+            <nav className="flex sticky top-0 z-[9999] px-[120px] py-[5px] bg-white/80 flex flex-col items-center">
+                <div className="flex items-center gap-6 mt-2 justify-between w-full">
+
+                    <Link to="/landingPackage" className="flex items-center 1.25">
+                        <div className="flex justify-center items-center w-[32px] h-[32px] bg-[#1A73E8] text-white rounded-2 font-normal text-center text-[14px] leading-[32px]">
+                            B
+                        </div>
+                        <span className="flex text-[#111827] font-normal text-[16px]">
+                            BeliMudah
+                        </span>
+                    </Link>
+
+                    <div className="w-full flex items-center">
+                        <input
+                            type="text"
+                            className="flex-1 w-full h-10 px-[12px] border border-[#D1D5DB] rounded-l-2 outline-none"
+                            placeholder="Cari produk, merek, kategori..."
+                        />
+                        <button className="w-[38px] h-10 border-none bg-[#1A73E8] rounded-r-2 cursor-pointer flex justify-center items-center">
+                            <Search className="text-white" size={20} />
+                        </button>
+                    </div>
+
+                    <div className="flex items-center gap-[6px]">
+                        <div className="w-10 h-10 flex justify-center items-center cursor-pointer">
+                            <Bell className="text-[#374151]" size={22} />
+                        </div>
+
+                        <Link to="/profilemyorder" className="w-10 h-10 flex justify-center items-center cursor-pointer">
+                            <User className="text-[#374151]" size={22} />
+                        </Link>
+                    
+                        <div className="px-2 font-medium text-[#374151]">
+                            <span>Budi</span>
+                        </div>
+
+                        <div className="w-10 h-10 flex justify-center items-center cursor-pointer">
+                            <Heart className="text-[#374151]" size={22} />
+                        </div>
+
+                        <div className="w-10 h-10 flex justify-center items-center cursor-pointer">
+                            <ShoppingCart className="text-[#374151]" size={22} />
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="flex justify-center gap-2.5">
+                    <div className="flex items-center 1.25 p-2.5 cursor-pointer text-[14px]">
+                        <Menu className="text-[#374151]" size={14} />
+                        <label htmlFor='halaman'></label>
+                        <select name="halaman" id="halaman" onChange={handleNavigasi} defaultValue="">
+                            <option value="" disabled>Semua Kategori</option>
+
+                            <option value="/mainbrows">Main Brows</option>
+                            <option value="/maindetail">Main Detail</option>
+                            <option value="/maincart">Main Chart</option>
+                            <option value="/checkout1">CheckOut step-1</option>
+                            <option value="/checkout2">CheckOut step-2</option>
+                            <option value="/checkout3">CheckOut step-3</option>
+                            <option value="/checkoutsucces">CheckOut Finish</option>
+                            <option value="/profilemyorder">Profile My Order</option>
+                            <option value="/profilewishlist">Profile Wishlist</option>
+                        </select>
+                    </div>
+
+                    <Link to="/home" className="flex items-center 1.25 p-2.5 cursor-pointer text-[14px] no-underline color-inherit">
+                        💻 Elektronik
+                    </Link>
+                    <Link to="/about" className="flex items-center 1.25 p-2.5 cursor-pointer text-[14px] no-underline color-inherit">
+                        👗 Fashion
+                    </Link>
+                    <div className="flex items-center 1.25 p-2.5 cursor-pointer text-[14px]">🏠 Rumah & Dapur</div>
+                    <div className="flex items-center 1.25 p-2.5 cursor-pointer text-[14px]">💄 Kecantikan</div>
+                    <div className="flex items-center 1.25 p-2.5 cursor-pointer text-[14px]">⚽ Olahraga</div>
+                    <div className="flex items-center 1.25 p-2.5 cursor-pointer text-[14px]">📚 Buku & Alat Tulis</div>
+                    <div className="flex items-center 1.25 p-2.5 cursor-pointer text-[14px] text-red-500 font-semibold">🔥 Promo</div>
+                </div>
+            </nav>
+        </>
+    );
+}
---
+++ src/pages/MainBrowse.jsx
@@ -0,0 +1,138 @@
+import { ChevronRight } from 'lucide-react'
+import Card from './../componen/Card';
+
+export default function MainBrowse() {
+    return (
+        <>
+            <main className="flex flex-col w-full px-26 bg-[#F9FAFB] gap-6">
+                <h1 className="flex items-center gap-1 text-sm text-gray-500">
+                    <div>Beranda</div>
+                    <div className='text-gray-500 pt-1'>
+                        <ChevronRight size={16} />
+                    </div>
+                    <span>Toko</span>
+                </h1>
+                <h1 className="flex text-xl font-bold text-black-600">Semua Produk</h1>
+                <div className='grid grid-cols-[20%_75%] w-full gap-6'>
+
+                    {/* //bagian kiri */}
+                    <div className='flex flex-col text-gray-500'>
+                        <div className='flex flex-col gap-6'>
+                            <h1>Harga</h1>
+                            <div className='flex justify-between'>
+                                <span>Rp 0</span>
+                                <span>Rp 20.000.000</span>
+                            </div>
+                        </div>
+                        <div className='flex flex-col mt-6 gap-4 '>
+                            <h1 className="flex text-xl font-bold text-black">Merek</h1>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="checkbox" id='TechMaster' className=' w-4 h-4' />
+                                <label htmlFor="TechMaster">TechMaster</label>
+                            </div>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="checkbox" id='SoundWave' className=' w-4 h-4' />
+                                <label htmlFor="SoundWave">SoundWave</label>
+                            </div>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="checkbox" id='PhoneX' className=' w-4 h-4' />
+                                <label htmlFor="PhoneX">PhoneX</label>
+                            </div>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="checkbox" id='OptiCam' className=' w-4 h-4' />
+                                <label htmlFor="OptiCam">OptiCam</label>
+                            </div>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="checkbox" id='FashionID' className=' w-4 h-4' />
+                                <label htmlFor="FashionID">FashionID</label>
+                            </div>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="checkbox" id='SportPro' className=' w-4 h-4' />
+                                <label htmlFor="SportPro">SportPro</label>
+                            </div>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="checkbox" id='BrewMaster' className=' w-4 h-4' />
+                                <label htmlFor="BrewMaster">BrewMaster</label>
+                            </div>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="checkbox" id='GlowLab' className=' w-4 h-4' />
+                                <label htmlFor="GlowLab">GlowLab</label>
+                            </div>
+
+                        </div>
+                        <div className='flex flex-col mt-6 gap-4 '>
+                            <h1 className="flex text-xl font-bold text-black">Rating Minimum</h1>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="radio" id='Stoktersedia' name='rating' className='w-4 h-4 rounded-full accent-blue-600 cursor-pointer' />
+                                <label htmlFor="Stoktersedia">★★★★☆ ke atas</label>
+                            </div>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="radio" id='Stoktersedia' name='rating' className='w-4 h-4 rounded-full accent-blue-600 cursor-pointer' />
+                                <label htmlFor="Stoktersedia">★★★★☆ ke atas</label>
+                            </div>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="radio" id='Stoktersedia' name='rating' className='w-4 h-4 rounded-full accent-blue-600 cursor-pointer' />
+                                <label htmlFor="Stoktersedia">★★★★☆ ke atas</label>
+                            </div>
+
+                        </div>
+                        <div className='flex flex-col mt-6 gap-4 '>
+                            <h1 className="flex text-xl font-bold text-black">Ketersediaan</h1>
+
+                            <div className='flex justify-start items-center gap-2'>
+                                <input type="checkbox" id='Stoktersedia' className=' w-4 h-4' />
+                                <label htmlFor="Stoktersedia">Stok tersedia</label>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    {/* bagian kanan */}
+                    <div className='flex flex-col gap-4'>
+
+                        <div className='flex justify-between'>
+                            <span>18 produk ditemukan</span>
+                            <div className='flex justify-center items-center gap-2'>
+                                <span className=' text-gray-500 '>Urutkan:</span>
+                                <div className=' border-2 border-gray-300 bg-white px-2 py-1 rounded-2xl'>
+                                    <button >Paling Populer</button>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className=' grid grid-cols-4 gap-2 '>
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                        </div>
+                        <div className='px-56'>
+                            <div className='flex justify-center items-center py-2 px-4 border-2 border-b-blue-500 text-indigo-500 rounded-3xl'>Muat Lebih Banyak (6 produk lagi)</div>
+                        </div>
+
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
