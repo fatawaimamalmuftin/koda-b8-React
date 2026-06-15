7ff788d39a162fcf6d5c890b941a46a74f1e16ed
fatawaimamalmuftin <muftinmarmut@gmail.com>
2026-06-15 09:29:24 +0700

    docs: cleaning folder

--- BeliHemat-react/src/pages/CheckOut1.jsx
+++
@@ -1,9 +0,0 @@
-export default function CheckOut1() {
-    return (
-        <>
-            <main className="flex flex-col items-center min-h-screen bg-slate-100 gap-6">
-                <h1 className="text-4xl font-bold text-emerald-600">Ini Halaman Tentang (CheckOut1)</h1>
-            </main>
-        </>
-    )
-}
