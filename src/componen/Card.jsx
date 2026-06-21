f0900d7924062ad25fbdb91139c9d1e0cfd911b9
fatawaimamalmuftin <muftinmarmut@gmail.com>
2026-06-21 10:12:54 +0700

    feat: added create a router switching path

--- src/componen/Card.jsx
+++ src/componen/Card.jsx
@@ -1,15 +1,16 @@
+import { Link } from 'react-router-dom';
 import elektronikImg from '../assets/elektronik.png';
 
 export default function Card() {
     return (
-        <div className="w-full border border-gray-100 rounded-xl overflow-hidden bg-white hover:shadow-sm transition">
-            <div className="relative w-full h-[220px] bg-gray-50 flex items-center justify-center">
+        <Link to="/maindetail" className="w-full border border-gray-100 rounded-xl overflow-hidden bg-white hover:shadow-sm transition">
+            <div className="relative w-full h-55 bg-gray-50 flex items-center justify-center">
                 <div className="absolute top-2 left-2 z-10 bg-[#DC2626] text-white text-[9px] font-bold px-1.5 py-0.5 rounded-md">
                     -31%
                 </div>
                 <img src={elektronikImg} className="w-full h-full object-cover" />
             </div>
-            <div className="w-full p-3 flex flex-col justify-between h-[120px]">
+            <div className="w-full p-3 flex flex-col justify-between h-30">
                 <div>
                     <span className="text-[9px] text-[#9CA3AF] font-bold uppercase tracking-wider">SoundWave</span>
                     <h3 className="text-xs font-semibold text-[#111827] truncate mt-0.5">Headphone Wireless Premium</h3>
@@ -23,6 +24,6 @@ export default function Card() {
                     <span className="text-[10px] text-[#9CA3AF] line-through">Rp 562.500</span>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
