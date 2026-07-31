251dce5136503038e6f508c0f963ecbe6b00a66b
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-07-31 09:13:29 +0700

    feat: fix issue bug storage

---
+++ src/app/storage.js
@@ -0,0 +1,17 @@
+const storage = {
+  getItem(key) {
+    return Promise.resolve(localStorage.getItem(key));
+  },
+
+  setItem(key, value) {
+    localStorage.setItem(key, value);
+    return Promise.resolve();
+  },
+
+  removeItem(key) {
+    localStorage.removeItem(key);
+    return Promise.resolve();
+  },
+};
+
+export default storage;
